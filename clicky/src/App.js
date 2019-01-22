8832c01ef28efa75c1dc4482191d9d92a33be78c
RCLARK4958 <RICKDCLARK48@GMAIL.COM>
2019-01-22 13:58:33 -0700

    appjs

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -1,26 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer";
+import characters from "./characters.json";
 import './App.css';
 
+
+
 class App extends Component {
+  state = {
+    characters,
+    score: 0,
+    topScore: 0,
+    message: "Click an image to begin",
+    clicked: [],
+  };
+
+  reset = () => {
+    this.setState({
+      score: 0,
+      clicked: []
+    });
+  }
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      console.log(this.state.clicked)
+      this.setState({ 
+        characters: characters.sort(function(a, b) {return 0.5 - Math.random()}),
+        score: this.state.score + 1,
+        topScore: Math.max(this.state.topScore, this.state.score + 1),
+        message: "Correct choice! Keep clicking!",
+        clicked: this.state.clicked.concat(id) 
+      });
+    } else {
+      this.setState({ 
+        characters: characters.sort(function(a, b) {return 0.5 - Math.random()}),
+        message: "Oops! That image was already clicked. Try again"
+      });
+      return this.reset();
+    }
+  };
+
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <Navbar score={this.state.score} topScore={this.state.topScore}>
+          {this.state.message}
+        </Navbar>
+        <Jumbotron />        
+          {this.state.characters.map(characters => (
+          <Card
+            handleClick={this.handleClick}
+            id={characters.id}
+            key={characters.id}
+            name={characters.name}
+            image={characters.image}
+          />
+          ))}
+        <Footer/>
+      </Wrapper>
     );
   }
 }
